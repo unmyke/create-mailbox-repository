ceabd4009eb80ee89056af81ca9e3a3b7ba312b8
unmyke <unmyke@gmail.com>
2020-02-25 16:40:06 +0800

    feat: add package entry point

---
+++ lib/index.ts
@@ -0,0 +1,4 @@
+export { default, createMailbox, getMailboxes, dropMailboxes } from './mailbox'
+export { NotifyHook } from './create-notify-hook-list'
+export { Msg } from './create-send-mail'
+export { Predicate } from './create-predicate-list'
