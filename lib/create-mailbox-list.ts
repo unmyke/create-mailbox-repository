b0aa277c2628f1c3e7e3db63e3acd8cdf02c22a7
unmyke <unmyke@gmail.com>
2020-02-25 16:31:34 +0800

    feat: add mailbox inventory methods factory

---
+++ lib/create-mailbox-list.ts
@@ -0,0 +1,54 @@
+import createListHandlers, {
+  ListGetter,
+  ListSetter,
+  ListProcessor
+} from './create-list-handlers'
+import Mailbox from './mailbox'
+
+export type MailboxGetter = (name: string) => Mailbox
+export type MailboxListGetter = ListGetter<Mailbox>
+export type MailboxSetter = ListSetter<Mailbox>
+export type MailboxesProccesor = (
+  origMailbox: Mailbox,
+  newMailbox: Mailbox
+) => void
+
+const createMailboxList = (): {
+  getMailboxes: MailboxListGetter;
+  getMailboxByName: MailboxGetter;
+  addMailbox: MailboxSetter;
+  removeMailbox: MailboxSetter;
+  dropMailboxes: ListProcessor;
+} => {
+  const {
+    get: getMailboxes,
+    add: addMailbox,
+    remove: removeMailbox
+  } = createListHandlers<Mailbox>()
+
+  const getMailboxByName = (name: string): Mailbox => {
+    const mailboxes = getMailboxes()
+
+    return mailboxes.find((mailbox: Mailbox): boolean => {
+      return mailbox.getName() === name
+    })
+  }
+
+  const dropMailboxes = (): void => {
+    const mailboxes = getMailboxes()
+
+    mailboxes.forEach(({ disable }: Mailbox): void => {
+      disable()
+    })
+  }
+
+  return {
+    getMailboxes,
+    getMailboxByName,
+    addMailbox,
+    removeMailbox,
+    dropMailboxes
+  }
+}
+
+export default createMailboxList
