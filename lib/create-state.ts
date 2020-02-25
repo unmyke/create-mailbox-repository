8ae7349a62389f6ac55bd1fc82030668c69b15be
unmyke <unmyke@gmail.com>
2020-02-25 16:09:00 +0800

    feat: add mailbox state methods factory
    createState creates:
    * isEnabled getter (return boolean);
    * disable setter (switch to "DISABLED" state).

---
+++ lib/create-state.ts
@@ -0,0 +1,22 @@
+export enum STATE {
+  ENABLED = 'enabled',
+  DISABLED = 'disabled'
+}
+
+export type StatePredicate = () => boolean
+export type Disable = () => void
+
+const createState = (): { isEnabled: StatePredicate; disable: Disable } => {
+  let state: STATE = STATE.ENABLED
+
+  const isEnabled = (): boolean => {
+    return state === STATE.ENABLED
+  }
+  const disable = (): void => {
+    state = STATE.DISABLED
+  }
+
+  return { isEnabled, disable }
+}
+
+export default createState
